ce9931715c55410f5c7c2e13eddf9a3eba50b449
younusraza909 <younusraza909@gmail.com>
2021-01-27 18:52:51 -0800

    Integrated state in app

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,25 @@
-const Button = ({ color, text }) => {
+import PropTypes from "prop-types";
+
+const Button = ({ color, text, onClick }) => {
   return (
-    <button style={{ backgroundColor: color }} className="btn">
+    <button
+      style={{ backgroundColor: color }}
+      onClick={onClick}
+      className="btn"
+    >
       {text}
     </button>
   );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,14 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = () => {
+  const onClick = () => {
+    console.log("Click");
+  };
+
   return (
     <header className="header">
       <h1>Task Tracker</h1>
-      <Button color="green" text="Hello" />
+      <Button color="green" text="Hello" onClick={onClick} />
     </header>
   );
 };
