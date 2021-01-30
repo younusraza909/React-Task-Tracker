afbe4deeaa7927d0ba0ce4755b3f8b0ef8751e03
younusraza909 <younusraza909@gmail.com>
2021-01-29 20:46:33 -0800

    Used useLocation from react-router-dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ onAdd, showAdd }) => {
+  const location = useLocation();
   return (
     <header className="header">
       <h1>Task Tracker</h1>
-      <Button
-        color={showAdd ? "red" : "green"}
-        text={showAdd ? "Close" : "Add"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
